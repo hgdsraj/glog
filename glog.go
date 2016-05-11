b3c2a9832b88af6895683a96e8dc56f329b872da
Tarmigan Casebolt <tarmigan+git@gmail.com>
2016-05-11 00:04:30 -0700

    Change flags so it can be used in parallel with glog for testing

--- glog.go
+++ glog.go
@@ -397,12 +397,12 @@ type flushSyncWriter interface {
 }
 
 func init() {
-	flag.BoolVar(&logging.toStderr, "logtostderr", false, "log to standard error instead of files")
-	flag.BoolVar(&logging.alsoToStderr, "alsologtostderr", false, "log to standard error as well as files")
-	flag.Var(&logging.verbosity, "v", "log level for V logs")
-	flag.Var(&logging.stderrThreshold, "stderrthreshold", "logs at or above this threshold go to stderr")
-	flag.Var(&logging.vmodule, "vmodule", "comma-separated list of pattern=N settings for file-filtered logging")
-	flag.Var(&logging.traceLocation, "log_backtrace_at", "when logging hits line file:N, emit a stack trace")
+	flag.BoolVar(&logging.toStderr, "tlogtostderr", false, "log to standard error instead of files")
+	flag.BoolVar(&logging.alsoToStderr, "talsologtostderr", false, "log to standard error as well as files")
+	flag.Var(&logging.verbosity, "tv", "log level for V logs")
+	flag.Var(&logging.stderrThreshold, "tstderrthreshold", "logs at or above this threshold go to stderr")
+	flag.Var(&logging.vmodule, "tvmodule", "comma-separated list of pattern=N settings for file-filtered logging")
+	flag.Var(&logging.traceLocation, "tlog_backtrace_at", "when logging hits line file:N, emit a stack trace")
 
 	// Default stderrThreshold is ERROR.
 	logging.stderrThreshold = errorLog
